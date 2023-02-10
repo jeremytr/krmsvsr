f0c52f904ed4e1e7e9142fc018a3f6f5d33a79c4
jeremy <=>
2023-02-10 11:08:58 +0300

    First Touch

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String veri = "";
+
+        System.out.println(hepHelloWorld(veri));
+    }
+    public  static String hepHelloWorld(String veri) {
+        veri = "Hello World";
+        return veri;
+    }
+}
